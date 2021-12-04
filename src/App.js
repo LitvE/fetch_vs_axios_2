7239f6caec3e926b537952e51232542ec0d5257c
LitvE <yevgen.litvinov1980@gmail.com>
2021-12-04 10:34:09 +0200

    error treatment

--- src/App.js
+++ src/App.js
@@ -6,13 +6,31 @@ const options = {
   page: 1,
   seed: 'abc',
 }
-
+/*
 function loadUsers({results, page, seed}){
   fetch(`https://randomuser.me/api/?results=${results}&seed=${seed}&page=${page}`).then((response) => response.json()).then(({results}) => console.dir(results)).then((error) => console.log(error));
+}*/
+
+function loadUsers({results, page, seed}){
+  fetch(`https://randomuser.me/api/?results=${results}&seed=${seed}&page=${page}`)
+  .then((response) => {
+    if(!response.ok) throw new Error(response.statusText);
+    response.json();
+  })
+  .then(({results}) => console.dir(results))
+  .catch((error) => console.log(error));
+}
+
+const configAxios = {
+  onUploadProgress: event => console.log(event.loaded),
+  onDownloadProgress: event => console.log(event.loaded),
+  timeout: 2000,
 }
 
 function loadUsersAxio ({results, page, seed}){
-  (axios.get(`https://randomuser.me/api/?results=${results}&seed=${seed}&page=${page}`)).then((response) => console.log(response.data.results)).catch((error) => console.log(error));
+  (axios.get(`https://randomuser.me/api/?results=${results}&seed=${seed}&page=${page}`, configAxios))
+  .then((response) => console.log(response.data.results))
+  .catch((error) => console.log(error.message));
 }
 
 function App() {
