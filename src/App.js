75a517b286a04d7c33bc4a846b5bee7dff0349b2
LitvE <yevgen.litvinov1980@gmail.com>
2021-12-04 10:13:26 +0200

    axios example

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import './App.css';
 
 const options = {
@@ -10,8 +11,14 @@ function loadUsers({results, page, seed}){
   fetch(`https://randomuser.me/api/?results=${results}&seed=${seed}&page=${page}`).then((response) => response.json()).then(({results}) => console.dir(results)).then((error) => console.log(error));
 }
 
+function loadUsersAxio ({results, page, seed}){
+  (axios.get(`https://randomuser.me/api/?results=${results}&seed=${seed}&page=${page}`)).then((response) => console.log(response.data.results)).catch((error) => console.log(error));
+}
+
 function App() {
-  loadUsers(options);
+  //loadUsers(options);
+  loadUsersAxio(options);
+
   return (
     <div className="App">
       <p>Load data from randomuser</p>
